3f94914b9a9ad4982fe511762322c29b284165da
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-02-07 11:18:15 +0200

    projectstusix

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,7 +1,5 @@
 import Swiper from 'swiper';
-
 import 'swiper/css';
-
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     spaceBetween: 30,
@@ -27,34 +25,21 @@ var swiper = new Swiper('.swiper-container', {
   const prevButton = document.querySelector('.swiper-button-prev');
     
   function updateNavigationButtons() {
-
     nextButton.addEventListener('click',()=>swiper.slideNext());
     prevButton.addEventListener('click',()=>swiper.slidePrev());
-  
-  
+
     if (swiper.isEnd) {
-  
-      nextButton.classList.add('swiper-button-disabled');
-  
-    } else {
-  
+       nextButton.classList.add('swiper-button-disabled');
+      } else {
       nextButton.classList.remove('swiper-button-disabled');
-  
+      }
+       if (swiper.isBeginning) {
+        prevButton.classList.add('swiper-button-disabled');
+      } else {
+        prevButton.classList.remove('swiper-button-disabled');
+      }
     }
-  
-  
-    if (swiper.isBeginning) {
-  
-      prevButton.classList.add('swiper-button-disabled');
-  
-    } else {
-  
-      prevButton.classList.remove('swiper-button-disabled');
-  
-    }
-  
-  }
-  
-   
+    
   updateNavigationButtons();
   
+
