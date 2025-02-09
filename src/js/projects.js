7e6cf41726359e2f8639a86b0202f147ff0f5f96
Tetyana_Dmytryienko <dmitrienko_tanya@ukr.net>
2025-02-09 11:46:49 +0200

    correctjs

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,8 +1,10 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     spaceBetween: 30,
+    centeredSlidesPerView: true,
     loop: false,
     keyboard: {
         enabled: true,
@@ -14,30 +16,27 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     },
     on: {
-    slideChange: function () {
-    updateNavigationButtons();
-    },
-    },
-  });
-  const nextButton = document.querySelector('swiper-button-next');
-  const prevButton = document.querySelector('.swiper-button-prev');
-    
-  function updateNavigationButtons() {
-    nextButton.addEventListener('click',()=>swiper.slideNext());
-    prevButton.addEventListener('click',()=>swiper.slidePrev());
-
-    if (swiper.isEnd) {
-       nextButton.classList.add('swiper-button-disabled');
+      slideChangeTransitionEnd: function () {
+      const nextButton = document.querySelector('.swiper-button-next');
+      const prevButton = document.querySelector('.swiper-button-prev');
+      if (swiper.isEnd) {
+        nextButton.disabled = true;
       } else {
-      nextButton.classList.remove('swiper-button-disabled');
+        nextButton.disabled = false;
       }
-       if (swiper.isBeginning) {
-        prevButton.classList.add('swiper-button-disabled');
+      if (swiper.isBeginning) {
+        prevButton.disabled = true;
       } else {
-        prevButton.classList.remove('swiper-button-disabled');
+        prevButton.disabled = false;
       }
     }
-    
-  updateNavigationButtons();
+        
+    },
+    },
+  );
   
-
+  
+    
+    
+ 
+  
